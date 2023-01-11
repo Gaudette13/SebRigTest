5395bdd5d6a41fe651c43504d5fc552da6563663
Gaudette13 <116587174+Gaudette13@users.noreply.github.com>
2023-01-11 11:00:19 -0500

    Update test_ttl.py

--- test_ttl.py
+++ test_ttl.py
@@ -10,7 +10,7 @@ ttl_trigger_PIN = 15
 ttl_marker_PIN = 27
 
 # PULSE LENGTH
-pulse_length = 3000
+pulse_length = 3
 
 # INITIALIZE PINS
 GPIO.setup(ttl_trigger_PIN, GPIO.OUT)
@@ -24,6 +24,7 @@ while infiniteloop > 0:
   GPIO.output(ttl_trigger_PIN, True)
   GPIO.output(ttl_marker_PIN, True)
   print("both on")
+  
   time.sleep(pulse_length)
   GPIO.output(ttl_trigger_PIN, False)
   GPIO.output(ttl_marker_PIN, False)
